346ac4f1e49327d879cfe465a0645148ce7abdbb
briezeba <bri_ezeba@hotmail.com>
2019-07-01 16:32:49 -0300

    Clases requeridas para la conexion Bluetooth

---
+++ Android/Codigo/trunk/app/src/main/java/com/example/fitvending/BtConnectionService.java
@@ -0,0 +1,215 @@
+package com.example.fitvending;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class BtConnectionService extends Service {
+
+    public TextView IdBufferIn;
+    public static String buffercito;
+    //-------------------------------------------
+    static Handler bluetoothIn;
+    final static int handlerState = 0;
+    private BluetoothAdapter btAdapter;
+    private static BluetoothSocket btSocket;
+    private static StringBuilder DataStringIN = new StringBuilder();
+    public static ConnectedThread MyConexionBT;
+    // Identificador unico de servicio - SPP UUID
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    // String para la direccion MAC
+    private static String address = null;
+
+    public static String Stock;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //CONSULTO EL STOCK DE LOS PRODUCTOS
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {
+                    String readMessage = (String) msg.obj;
+                    DataStringIN.append(readMessage);
+                    buffercito=readMessage;
+                    //Toast.makeText(getBaseContext(), readMessage, Toast.LENGTH_LONG).show();
+                    int endOfLineIndex = DataStringIN.indexOf("#");
+
+                    if (endOfLineIndex > 0) {
+                        String dataInPrint = DataStringIN.substring(0, endOfLineIndex);
+                        //IdBufferIn.setText(dataInPrint);
+                            Stock = dataInPrint;
+
+                        DataStringIN.delete(0, DataStringIN.length());
+                    }
+                }
+            }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter(); // get Bluetooth adapter
+       // VerificarEstadoBT();
+
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        address = "98:D3:51:FD:A1:E3"; //intent.getStringExtra(DispositivosBT.EXTRA_DEVICE_ADDRESS);//<-<- PARTE A MODIFICAR >->->
+        //Setea la direccion MAC
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try
+        {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "La creacción del Socket fallo", Toast.LENGTH_LONG).show();
+        }
+        //Establece la conexión con el socket Bluetooth.
+        try
+        {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {}
+        }
+        MyConexionBT = new ConnectedThread(btSocket);
+        MyConexionBT.start();
+
+        //Consultar Stock
+        MyConexionBT.write("0");
+
+
+        return START_NOT_STICKY;
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException
+    {
+        //crea un conexion de salida segura para el dispositivo
+        //usando el servicio UUID
+        return device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+    }
+
+
+    //Comprueba que el dispositivo Bluetooth Bluetooth está disponible y solicita que se active si está desactivado
+    private void VerificarEstadoBT() {
+
+        if(btAdapter==null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+
+            }
+        }
+
+
+    }
+
+    public static void consultarStock(){
+
+        if(btSocket.isConnected())
+        enviarDatosAArduino("0");
+        else
+           Stock = "1-0-1-1";
+
+
+    }
+
+
+    public static void enviarDatosAArduino(String datoAEnviar){
+
+        if(btSocket.isConnected())
+        MyConexionBT.write(datoAEnviar);
+
+    }
+
+
+    public static void detenerBt(){
+        try {
+            btSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //Crea la clase que permite crear el evento de conexion
+    private class ConnectedThread extends Thread
+    {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket)
+        {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try
+            {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run()
+        {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            // Se mantiene en modo escucha para determinar el ingreso de datos
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Envia los datos obtenidos hacia el evento via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+        //Envio de trama
+        public void write(String input)
+        {
+            try {
+                mmOutStream.write(input.getBytes());
+            }
+            catch (IOException e)
+            {
+                //si no es posible enviar datos se cierra la conexión
+                Toast.makeText(getBaseContext(), "La Conexión fallo", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
+
+
+
+
+}
