1726469fcc21342b0767fca4ba2937360f855179
briezeba <bri_ezeba@hotmail.com>
2019-07-01 16:29:55 -0300

    Se agrega pantalla de carga

--- Android/Codigo/trunk/app/src/main/java/com/example/fitvending/PantallaDeCarga.java
+++ Android/Codigo/trunk/app/src/main/java/com/example/fitvending/PantallaDeCarga.java
@@ -1,30 +1,53 @@
 package com.example.fitvending;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class PantallaDeCarga extends AppCompatActivity {
 
-    private int tiempo = 3000;
+    private int tiempo = 6000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_de_carga);
 
-        //Aca poner los permisos
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter(); // get Bluetooth adapter
+
+        if(btAdapter==null) {
+            Toast.makeText(getBaseContext(), "El dispositivo no soporta bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
 
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run(){
-                // Cuando pasen los 3 segundos, pasamos a la actividad principal de la aplicación
 
-                Intent intent = new Intent(PantallaDeCarga.this, MainActivity.class);
+                Intent intent = new Intent(PantallaDeCarga.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             };
         }, tiempo);
     }
+
+
+    public void onResume(){
+
+        super.onResume();
+
+        Intent service = new Intent(getBaseContext(),BtConnectionService.class);
+        startService(service);
+
+
+    }
+
 }
